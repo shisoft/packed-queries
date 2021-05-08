010f66810d2847ce3899596716e62da6ec372379
Hao Shi <shisoftgenius@gmail.com>
2021-05-08 05:05:01 -0400

    clustering log

--- src/main.rs
+++ src/main.rs
@@ -569,6 +569,7 @@ fn to_ndarray(data: &DataSet) -> Array2<f32> {
 }
 
 fn clustering(data: &DataSet, k: usize) -> Clusters {
+    println!("Clustering with k {}", k);
     let ndarray = to_ndarray(data);
     let obversations = DatasetBase::from(ndarray);
     let rand = StdRng::from_rng(thread_rng()).unwrap();
